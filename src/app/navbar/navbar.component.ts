733a4b100663577ec4ddf83241d72b0117e9db0b
Josue <96113041+Rowada@users.noreply.github.com>
2023-03-21 11:54:23 +0100

    feat: update navbar and add logout function

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StorageService } from '../_services/storage.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  constructor(private storageService: StorageService, private router: Router) {}
+
+  ngOnInit(): void {}
+
+  get isLoggedIn() {
+    return this.storageService.isLoggedIn();
+  }
+  logout() {
+    sessionStorage.removeItem('auth-user');
+    return this.router.navigateByUrl('/login');
+  }
   showMenu = false;
-  toggleNavbar(){
+  toggleNavbar() {
     this.showMenu = !this.showMenu;
+  }
+
+  reloadPage(): void {
+    window.location.reload();
   }
 }
