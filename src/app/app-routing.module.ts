373b0f8ed834d48688418abf69250e1f6feeaf02
Josue <96113041+Rowada@users.noreply.github.com>
2023-03-23 20:55:48 +0100

    fix: debug routerlink for dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,12 +7,14 @@ import { AboutComponent } from './component/about/about.component';
 import { UserProfileComponent } from './pages/user-profile/user-profile.component';
 import { DashboardComponent } from './component/dashboard/dashboard.component';
 import { canActivate } from './guards/auth.guard';
+import { BankAboutComponent } from './pages/bank-about/bank-about.component';
+import { ClientDashboardComponent } from './pages/client-dashboard/client-dashboard.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'about', component: AboutComponent },
+  { path: 'about', component: BankAboutComponent },
   {
     path: 'profile',
     component: UserProfileComponent,
@@ -20,7 +22,7 @@ const routes: Routes = [
   },
   {
     path: 'dashboard',
-    component: DashboardComponent,
+    component: ClientDashboardComponent,
     canActivate: [canActivate],
   },
 ];
